444e31dffb8a4ec9b0ca8bc207521bf491541e82
EngMhdAmmar <n.ammarnw6@gmail.com>
2024-04-07 21:59:59 +0300

    validation

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -12,17 +12,13 @@ use Illuminate\Support\Facades\Validator;
 
 class ChatController extends Controller
 {
-    public function send(Request $request){
-        $validator = Validator::make($request->all(), [
-            'message' => ['required', 'string'],
-            'receiver' => ['required'],
-        ]);
-        if ($validator->fails()) {
-            return response()->json(
-                $validator->errors(),
-                403
-            );
-        }
+    public function send(Request $request) {
+        $rules = [
+            'message' => 'required|string',
+            'receiver' => 'required',
+        ];
+        $validator = Validator::make($request->all(),$rules);
+        if ($validator->fails()) return response()->json($validator->errors(), 403);
         $user = Auth::user();
         $receiver = User::find($request->receiver);
         $chat = Chat::where(function($query) use ($user, $receiver) {
@@ -32,13 +28,13 @@ class ChatController extends Controller
         })->first();
         if(!$chat) {
             $chat=Chat::create([
-                'f_user'=> $user->id,
-                's_user'=> $receiver->id,
+                'f_user' => $user->id,
+                's_user' => $receiver->id,
             ]);
         }
         $chat->messages()->create([
-            'sender_id'=> $user->id,
-            'receiver_id'=> $receiver->id,
+            'sender_id' => $user->id,
+            'receiver_id' => $receiver->id,
             'text' => $request->message,
             'status' => '0',
         ]);
@@ -46,20 +42,18 @@ class ChatController extends Controller
         return response()->json(['message'=>$request->message],200);
     }
 
-    public function receive_message(Request $request){
+    public function receive_message(Request $request) {
         Message::where('id',$request->id)->update([
             'status' => '1',
         ]);
         return response()->json(['message' => 'message received'],200);
     }
 
-    public function read_message($id){
+    public function read_message($id) {
         Message::where('id',$id)->update([
             'status' => '2',
         ]);
         return response()->json(['message' => 'the message has been read'],200);
     }
-
-
 
 }
